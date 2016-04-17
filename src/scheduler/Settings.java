1291a18109cd662b935830ff63c45c24afe20a2f
cliffplaysdrums <cliffplaysdrums@gmail.com>
2016-04-17 13:01:26 -0400

    have to

---
+++ src/scheduler/Settings.java
@@ -0,0 +1,16 @@
+
+package scheduler;
+
+/**
+ *
+ * @author Cliff
+ */
+public class Settings {
+    boolean autoLogon = false;
+    java.awt.Color backgroundColor = new java.awt.Color(204, 255, 204);
+    java.awt.Color foregroundColor = new java.awt.Color(51, 51, 51);
+    
+    public Settings () {
+        
+    }
+}
